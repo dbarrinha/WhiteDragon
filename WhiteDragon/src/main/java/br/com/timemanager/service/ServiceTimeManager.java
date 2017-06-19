2d579d60f25f08057f9ac6bbd322d3049cdbc483
danilo.barrinha <danilo.barrinha@DISAU-07122.tce.pi.gov.br>
2017-06-19 12:23:55 -0300

    web servidor

--- WhiteDragon/src/main/java/br/com/timemanager/service/ServiceTimeManager.java
+++ WhiteDragon/src/main/java/br/com/timemanager/service/ServiceTimeManager.java
@@ -1,18 +1,24 @@
 package br.com.timemanager.service;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Path;
 
-@Path("/timeservice")
-public class ServiceTimeManager {
-	
-	
-	
-	public String startSignature(@PathParam("docId") String docId) throws RestException{
-		
-		Long accountabilityItemId = Long.parseLong(docId);
-		
-		String token = assinaturaDigitalService.calcularHash(accountabilityItemFacade.getFileContent(accountabilityItemId));
-		
-		return "\"" + token + "\"";
+@WebServlet(value="/webServer", name="ServletTimeManager")
+public class ServiceTimeManager extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String nome = request.getParameter("nome");
+		System.out.println(nome);
+		PrintWriter out = response.getWriter();
+		out.println("String enviada via get: " + nome);
 	}
 }

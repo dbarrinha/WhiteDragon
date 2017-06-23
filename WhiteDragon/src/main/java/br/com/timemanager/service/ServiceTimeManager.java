2782c6eeb51fb3c6e867ea503eb89438dfbad2d4
Computacao2014 <cc2014.1@outlook.com>
2017-06-23 08:18:46 -0300

    hfghj

--- WhiteDragon/src/main/java/br/com/timemanager/service/ServiceTimeManager.java
+++ WhiteDragon/src/main/java/br/com/timemanager/service/ServiceTimeManager.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Path;
 
 @WebServlet(value="/webServer", name="ServletTimeManager")
 public class ServiceTimeManager extends HttpServlet {
